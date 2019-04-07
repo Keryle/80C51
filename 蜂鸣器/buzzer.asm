4510b755c22fa16acaa18a5f7b970c3d3d1e1991
Keryle <m_caohaijun@163.com>
2019-04-07 11:56:51 +0800

    s

--- 蜂鸣器/buzzer.asm
+++ 蜂鸣器/buzzer.asm
@@ -5,10 +5,10 @@ ljmp _main
 .org 0x000b
 ljmp _TimerIntterupt0
 _main:
-  mov _TMOD,0x00  ;定时器0，方式0，13位定时器
+  mov _TMOD,#0x00  ;定时器0，方式0，13位定时器
   setb _EA
   setb _ET0
-  mov a,0x00
+  mov a,#0x00
   lcall _FindStartT0
 01$:
   sjmp 01$
@@ -16,17 +16,17 @@ _main:
 
 _TimerIntterupt0:
   clr _TR0
-  mov a,0x00
+  mov a,#0x00
   lcall _FindStartT0
   cpl _P1_5               ;取反输出波形
 
 
 
 _FindStartT0:
-  mov dptr,0x1000
+  mov dptr,#0x1000
   movc a,@a+dptr
   mov _TL0,a            ;查表赋值低位
-  mov dptr,0x1010
+  mov dptr,#0x1010
   movc a,@a+dptr
   mov _TH0,a            ;查表赋值高位
   setb _TR0             ;启动定时器0
