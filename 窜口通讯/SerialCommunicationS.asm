9d2ca17b96d0dde13aefa0afc7ba89292ecc17c5
Keryle <m_caohaijun@163.com>
2019-04-08 17:26:43 +0800

    /

--- 窜口通讯/SerialCommunicationS.asm
+++ 窜口通讯/SerialCommunicationS.asm
@@ -2,15 +2,14 @@
 .area home (abs)
 .org 0x0000
 ljmp _main
-.org 0x0003
-ljmp _InttX0
-.org 0x0023
+org 0x0023
+ljmp _InttS
 
 
 _main:
   setb _EA
-  setb _EX0
-  mov _SCON,0x40     ;way1
+  setb _ES
+  mov _SCON,0x50     ;way1
   mov _PCON,0x80     ;double
   mov _TMOD,0x20     ;T2-->way2
   mov _TH1,0xf3
@@ -19,6 +18,7 @@ _main:
 01$:
   sjmp 01$
 
-_InttX0:
-  mov _SBUF,0xf9
+_InttS:
+  mov _P2,0xf9
+  clr _RI
   reti
