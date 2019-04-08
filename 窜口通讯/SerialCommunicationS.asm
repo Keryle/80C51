3721c022dd0458d29476cb4e0d84311d601fb142
Keryle <m_caohaijun@163.com>
2019-04-08 19:37:19 +0800

    .

--- 窜口通讯/SerialCommunicationS.asm
+++ 窜口通讯/SerialCommunicationS.asm
@@ -9,16 +9,17 @@ ljmp _InttS
 _main:
   setb _EA
   setb _ES
-  mov _SCON,0x50     ;way1
-  mov _PCON,0x80     ;double
-  mov _TMOD,0x20     ;T2-->way2
-  mov _TH1,0xf3
-  mov _TL1,0xf3      ;4800
+  mov _SCON,#0x50     ;way1
+  mov _PCON,#0x00     ;double
+  mov _TMOD,#0x20     ;T2-->way2
+  mov _TH1,#0xf3
+  mov _TL1,#0xf3      ;4800
   setb _TR1
+
 01$:
   sjmp 01$
 
 _InttS:
-  mov _P2,0xf9
   clr _RI
+  mov _P2,_SBUF
   reti
