5ba329dd7ea0a582c290f4ba81e82a0e57055b71
Keryle <m_caohaijun@163.com>
2019-04-09 13:51:21 +0800

    0

--- 矩阵键盘/main.asm
+++ 矩阵键盘/main.asm
@@ -41,7 +41,12 @@ _KeyDown:
   mov r2,#3
   sjmp 20$
 05$:
+  cjne a,#0x0e,06$
   mov r2,#4
+  sjmp 20$
+06$:
+  setb 0x10
+  ret                      ;可能同时有多个按键按下，置位报错
 20$:
   mov _P1,#0xf0            ;判断列，高电平被拉低
   mov a,_P1
@@ -49,21 +54,26 @@ _KeyDown:
   mov r3,#4
   sjmp 30$                 ;第4列
 13$:
-  cjne a,#0x0b,14$
+  cjne a,#0xb0,14$
   mov r3,#3
   sjmp 30$
 14$:
-  cjne a,#0x0d,15$
+  cjne a,#0xd0,15$
   mov r3,#2
   sjmp 30$
 15$:
+  cjne a,#0xe0,16$
   mov r3,#1
+  sjmp 30$
+16$:
+  setb 0x10                 ;可能同时有多个按键按下，置位报错
+  ret
 30$:
   mov a,r2
   clr c
   subb r2,#1
   mov b,#4
-  mul a,b
+  mul ab
   add a,r3              ;求出按键的地址
   push a
   mov r7,#50
@@ -76,6 +86,15 @@ _KeyDown:
   lcall _delay
   djnz r7,41$:
   pop a
+  cjne a，#0x02，51$
   ret
-
+51$:
+  setb 0x11
+  ret
 _main:
+01$:
+  lcall _KeyDown
+  jnb #0x11,01$
+  cjne a,#0x01,01$
+  mov _P2,#0xf9
+  sjmp 01$
