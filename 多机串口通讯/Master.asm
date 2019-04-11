772f2480eef3f1870addc1b6c5836f0531a717dd
Keryle <m_caohaijun@163.com>
2019-04-11 20:38:03 +0800

    2

--- 多机串口通讯/Master.asm
+++ 多机串口通讯/Master.asm
@@ -1,9 +1,18 @@
 .include /asm51.h/
+.area home (abs)
 .org 0x0000
 ljmp _main
 
 .org 0x0030
-
+_main:
+    mov r0,#0x30
+    mov @r0,#0x11
+    mov r2,#0x01
+    mov r3,#0x00
+    mov r4,#0x01
+    lcall _SerialCom
+01$:
+    sjmp 01$
 ;-------------------------------------
 ;窜口通讯子程序
 ;入口参数：
@@ -14,7 +23,7 @@ ljmp _main
 ;r4：数据长度
 ;-------------------------------------
 
-_SerialCommunication：
+_SerialCom:
 ;定时器T1方式2 波特率2400 不加倍
     mov _TOMD,#0x20
     mov _TH1,#0xf3
@@ -25,42 +34,53 @@ _SerialCommunication：
 ;当SM2=1，发送第九位为1时，RI=1，串口中断
 ;发送第九位为0时，RI=0，不产生串口中断
 ;当SM2=0不管第九位，产生中断
-;设置主机为方式3，SM2=1，TB8=1
+;设置主机为方式3，SM2=0，TB8=1,允许接收
     mov _SCON,#0xd8
 
 90$:
     mov a,r2       ;发送地址
     mov _SBUF,a
-01$:jnb _TI,01$
+09$:jnb _TI,09$
 02$:jnb _RI,02$
     clr _RI
+    clr _TI
     mov a,_SBUF
     xrl a,r2
-    jz 03$          ;相等转发送命令
-91$:
+    jz 03$          ;应答地址相等转
+91$:                ;复位操作
     mov _SBUF,#0x00
     setb _TB8
     sjmp 90$
 03$:
-    clr _TB8         ;清除地址标志，双机通讯
+    clr _TB8         ;发送命令帧
     mov _SBUF,r3
 04$:jnb _TI,04$
 05$:jnb _RI,05$
     clr _RI
+    clr _TI
+    ;接收从机返回信号
+    ;判断接收或发送
+    ;发送
     mov a,_SBUF
-    cjne r3,#0x00,10$
-    cjne a,#0x01,20$
-    sjmp 91$
+    cjne r3,#0x00,10$;如果发送的命令是00H
+    cjne a,#0x01,91$ ;返回信号错误转到复位，否则发送数据
+11$:
+    mov _SBUF,@r0
+06$:jnb _TI,06$
+    clr _TI
+    inc r0
+    djnz r4,11$
+    ret
+    ;接收
 10$:
-    cjne r3,#0xff,80$;输入控制命令错误
-    cjne a,#0x02,20$
-    sjmp 91$
-20$:
-    mov _SBUF,#0xf0     ;主机确认指令
-04$:jnb _TI,04$
-05$:jnb _RI,05$
-
-
-
-
-_main:
+    cjne r3,#0xff,80$;转到控制命令错误
+    cjne a,#0x02,91$ ;返回信号错误转到复位，否则接收数据
+07$:jnb _RI,07$
+    clr _RI
+    mov @r1,_SBUF
+    inc r1
+    djnz r4,07$
+    ret
+80$:
+    setb 0x00
+    ret
---
+++ 多机串口通讯/Slave1.asm
@@ -0,0 +1,74 @@
+.include /asm51.h/
+.area home (abs)
+.org 0x0000
+ljmp _main
+.org 0x0023
+ljmp _SerialIntt
+
+.org 0x0030
+_main:
+    mov _SP,#0x60   ;堆栈指针
+    mov _SCON,#0xf0 ;方式3，允许接收，SM2=1
+    mov _TOMD,#0x20
+    mov _TH1,#0xf3
+    mov _TL1,#0xf3
+    setb _TR1
+    mov _PCON,#0x00
+    setb _EA
+    setb _ES
+    mov 0x08,#0x40 ;1组r0,接收
+    mov 0x09,#0x50 ;1组r1,发送
+    mov 0x0a,#0x01 ;1组r2,字节数
+01$:
+    sjmp 01$
+
+_SerialIntt:
+    push a
+    push _PSW
+    setb _RS0
+    clr _RS1
+    mov a,_SBUF
+    orl a,#0x01
+    jz 01$
+90$:                ;返回
+    pop _PSW
+    pop a
+    clr _RS0
+    setb _SM2
+    reti
+01$:                ;地址相等,发送本机地址
+    clr _SM2
+    mov _SBUF,#0x01
+101$:
+    jnb _TI,101$
+102$:
+    jnb _RI,102$
+    clr _RI         ;接收命令帧
+    clr _TI
+    mov a,_SBUF
+    jnb _RB8,90$
+    ;判断命令帧类型
+    ;接收
+    cjne a,#0x00,10$
+    mov _SBUF,#0x01
+103$:
+    jnb _TI,103$
+02$:jnb _RI,02$
+    clr _RI
+    mov @r0,_SBUF
+    inc r0
+    clr _P3_7
+    djnz r2,02$
+    sjmp 90$        ;退出
+
+    ;发送
+10$:
+    cjne a,#0xff,90$
+    mov _SBUF,#0x02
+104$:
+    jnb _TI,104$
+    clr _TI
+    mov _SBUF,@r1
+    inc r1
+    djnz r2,104$
+    sjmp 90$
